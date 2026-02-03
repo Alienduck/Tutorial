6ec077ed543c2ffe3a71d79d35563cb60970f96d
Owen Neuillyr <jacquonneries@gmail.com>
2026-02-03 16:24:17 +0100

    fix connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import db from './db/db.js';
-import mongoose from 'mongoose';
-import userRoute from './routes/user.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import db from "./db/db.js";
+import userRoute from "./routes/user.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // TODO: Change in prod
+    credentials: true
+}))
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB error:", err));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
 
 db();
 
-app.use('/user', userRoute)
+app.use('/user', userRoute);
